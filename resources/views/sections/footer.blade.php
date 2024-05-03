ccdfc13044c94675963b9a370f1dbd40afec1252
Nguyễn Tài Tâm <taitam8590@gmail.com>
2024-05-03 23:22:57 +0700

    add layout

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,3 +1,12 @@
-<footer class="content-info">
-  @php(dynamic_sidebar('sidebar-footer'))
+<footer class="bg-black text-center py-5">
+    <div class="container px-5">
+        <div class="text-white-50 small">
+            <div class="mb-2">&copy; Your Website 2023. All Rights Reserved.</div>
+            <a href="#!">Privacy</a>
+            <span class="mx-1">&middot;</span>
+            <a href="#!">Terms</a>
+            <span class="mx-1">&middot;</span>
+            <a href="#!">FAQ</a>
+        </div>
+    </div>
 </footer>
