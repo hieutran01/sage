478338ab3e803f52572b0b91deb68f2821a270d4
taitamfc <taitam8590@gmail.com>
2024-05-04 21:03:07 +0700

    Update index.blade.php

--- resources/views/schedules/index.blade.php
+++ resources/views/schedules/index.blade.php
@@ -4,16 +4,16 @@
     <div class="container">
         <ul class="nav nav-pills mb-3 justify-content-start" id="pills-tab" role="tablist">
             <li class="nav-item" role="presentation">
-                <button type="button" onclick="loadSchedule('',this)" class="nav-link active" >Tất cả</button>
+                <button data-type="" type="button" class="loadSchedule nav-link active" >Tất cả</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button type="button" onclick="loadSchedule({{ \App\Models\Schedule::IS_LIVE }},this)" class="nav-link">Trực tiếp(<?= $number_live ;?>)</button>
+                <button data-type="{{ \App\Models\Schedule::IS_LIVE }}" type="button" class="loadSchedule nav-link">Trực tiếp(<?= $number_live ;?>)</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button type="button" onclick="loadSchedule({{ \App\Models\Schedule::FINISHED }},this)" class="nav-link" >Đã kết thúc</button>
+                <button data-type="{{ \App\Models\Schedule::FINISHED }}" type="button" class="loadSchedule nav-link" >Đã kết thúc</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button type="button" onclick="loadSchedule({{ \App\Models\Schedule::NOT_START }},this)" class="nav-link" >Lịch thi đấu</button>
+                <button data-type="{{ \App\Models\Schedule::NOT_START }}" type="button" class="loadSchedule nav-link" >Lịch thi đấu</button>
             </li>
         </ul>
         <div class="tab-content" id="pills-tabContent">
