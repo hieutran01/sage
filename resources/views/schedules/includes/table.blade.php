386037fe0101ef253fcaddf743cd58459a1a62b9
taitamfc <taitam8590@gmail.com>
2024-05-04 21:03:37 +0700

    Update table.blade.php

--- resources/views/schedules/includes/table.blade.php
+++ resources/views/schedules/includes/table.blade.php
@@ -2,8 +2,9 @@
     @foreach ( $items_arr as $sclass_id =>  $items)
         <tr data-id="{{$sclass_id}}" class="sclasse sclasse_{{$sclass_id}}" style="background-color: #ddd;">
             <td class="lighter-color">
-                <i onclick="addFavorite({{$sclass_id}},'sclass')" 
-                class="curson-poiter favoriteScheduleSclass favoriteScheduleSclass_{{$sclass_id}} far fa-star {{ in_array($sclass_id,$favorites['sclass']) ? 'fas' : '' }}"></i>
+                <i 
+                data-item_id="{{$sclass_id}}" data-type="sclass" 
+                class="addFavorite curson-poiter favoriteScheduleSclass favoriteScheduleSclass_{{$sclass_id}} far fa-star {{ in_array($sclass_id,$favorites['sclass']) ? 'fas' : '' }}"></i>
             </td>
             <td><img src="{{ $sclasses[$sclass_id]->sclass_pic }}" alt="Sclass Pic" width="25"></td>
             <td colspan="6">{{ $sclasses[$sclass_id]->name_e}}</td>
@@ -11,8 +12,8 @@
         @foreach ( $items as $item)
         <tr data-id="{{$item->schedule_id}}" class="schedule schedule_{{ $item->schedule_id }} belongTo_sclasse_{{$sclass_id}}">
             <td class="lighter-color">
-                <i onclick="addFavorite({{$item->schedule_id}},'match')" 
-                class="curson-poiter favoriteScheduleMatch favoriteScheduleMatch_{{$item->schedule_id}} far fa-star  {{ in_array($item->schedule_id,$favorites['match']) ? 'fas' : '' }}"></i></td>
+                <i data-item_id="{{$item->schedule_id}}" data-type="match" 
+                class="addFavorite curson-poiter favoriteScheduleMatch favoriteScheduleMatch_{{$item->schedule_id}} far fa-star  {{ in_array($item->schedule_id,$favorites['match']) ? 'fas' : '' }}"></i></td>
             <td id="match_time_{{ $item->schedule_id }}" class="lighter-color">{{ $item->match_time_fm}}</td>
             <td id="match_minute_{{ $item->schedule_id }}"> <span style="color: red;">{{ $item->match_minutes_fm}}</td>
             <td class="text-end">
